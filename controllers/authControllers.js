b7dd039740c2cef5b293ec32dbda70463dd78a79
aloksisodiya <aloksisodiya30@gmail.com>
2025-05-31 22:27:00 +0530

    added routes,controllers folder and modidified index.js

---
+++ controllers/authControllers.js
@@ -0,0 +1,13 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const db = require('../db/db.js');
+
+const register = async(req,res) => {
+
+}
+
+const login = async(req,res) => {
+
+}
+
+module.exports = {register,login}
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const router = require('./routes');
+const router = require('./routes/authRoute');
 
 const app = express();
 app.use(express.json());
-app.use(router);
+app.use('/api/auth',router);
 
 app.listen(3000, ()=> console.log('server listening on port 3000'));
