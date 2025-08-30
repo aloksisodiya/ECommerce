f6603728fa25fd3317f52fa1d8ec32a02fec4190
aloksisodiya <aloksisodiya30@gmail.com>
2025-08-30 20:05:55 +0530

    feat: api doc for auth

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,7 +3,72 @@ import { register,login } from '../controllers/auth.controllers.js';
 
 const registerRouter = express.Router();
 
+/**
+ * @swagger
+ * /api/auth/register:
+ *   post:
+ *     tags:
+ *       - Auth
+ *     summary: Register a User
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/x-www-form-urlencoded:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: User's name
+ *                 example: ""
+ *               email:
+ *                 type: string
+ *                 description: User's email
+ *                 example: ""
+ *               password:
+ *                 type: string
+ *                 description: User's password
+ *                 example: ""
+ *     responses:
+ *       '200':
+ *         description: added Successfully
+ *       '500':
+ *         description: server error
+ */
+
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     tags:
+ *       - Auth
+ *     summary: Login a User
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/x-www-form-urlencoded:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: User's email
+ *                 example: ""
+ *               password:
+ *                 type: string
+ *                 description: User's password
+ *                 example: ""
+ *     responses:
+ *       '200':
+ *         description: Login Successful
+ *       '401':
+ *         description: Unauthorized
+ *       '500':
+ *         description: server error
+ */
 registerRouter.post('/register',register);
+
+
 registerRouter.post('/login',login);
 
 export default registerRouter;
